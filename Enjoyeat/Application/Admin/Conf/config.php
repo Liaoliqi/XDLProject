a73c1c521970df5d7feb62ea24291df86dc6e8bf
kid <kid@163.com>
2016-06-22 21:38:00 +0800

    添加后台登入功能

--- Enjoyeat/Application/Admin/Conf/config.php
+++ Enjoyeat/Application/Admin/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
 	/* 数据库设置 */
     'DB_TYPE'               =>  'Mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  's45_student',          // 数据库名
-    'DB_USER'               =>  'hdy',      // 用户名
+    'DB_HOST'               =>  '192.168.30.159', // 服务器地址
+    'DB_NAME'               =>  'enjoyeat',          // 数据库名
+    'DB_USER'               =>  'been',      // 用户名
     'DB_PWD'                =>  '123456',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_PREFIX'             =>  'enjoyeat_',    // 数据库表前缀
 );
---
+++ Enjoyeat/Application/Admin/Controller/PublicController.class.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Admin\Controller;
+
+class PublicController extends AdminController{
+	public function login()
+	{
+		$this->display();
+	}
+
+	public function doLogin()
+	{
+		$username=I('post.username');
+		$password=I('post.password');
+
+		$model = M('user_login');
+		$data = $model->where(array('username'=>$username))->find();
+		if(is_null($data)){
+			$this->error('您使用的用户不存在!');
+			exit;
+		}
+		if(md5($password)!== $data['password']){
+			$this->error('您输入的密码有误！！');
+			exit;
+		}
+
+		//回话开始
+		$_SESSION['user_info']=$data;
+
+		//成功后跳转首页
+
+		$this->redirect('Index/index','',3,'Loading.......');
+	
+
+	}
+
+		
+
+}
