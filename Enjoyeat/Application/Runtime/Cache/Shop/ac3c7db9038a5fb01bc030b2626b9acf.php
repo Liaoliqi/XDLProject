2559321e5d71ceafddd0468310564c61d2e7c6a9
Liaoliqi <1184055585@qq.com>
2016-06-23 11:41:17 +0800

    yinyin

---
+++ Enjoyeat/Application/Runtime/Cache/Shop/ac3c7db9038a5fb01bc030b2626b9acf.php
@@ -0,0 +1,33 @@
+<?php if (!defined('THINK_PATH')) exit();?><html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/XDLProject/Enjoyeat/Public/bootstrap/css/bootstrap.min.css" />
+	<style type="text/css">
+		body{
+			background-image: url("/XDLProject/Enjoyeat/Public/Shop/images/header-bg.jpg");
+			padding: 20px;
+			color: #eee;
+		}
+	</style>
+</head>
+<body>
+	<div class="container ">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<h2><img src="/XDLProject/Enjoyeat/Public/Shop/images/logosm.png">商家平台</h2>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<img src="/XDLProject/Enjoyeat/Public/Shop/images/header-promotion.png">
+				<h2>把您餐厅开上享吃</h2>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+</body>
+</html>
